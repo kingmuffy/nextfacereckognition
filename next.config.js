f6ece537fd8c877e8d27492ac00a5f4f3271a7aa
Ojei Omamofe <65783231+kingmuffy@users.noreply.github.com>
2023-09-24 12:33:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      output: 'export',
     images: {
       domains: ["images.pexels.com", "backguy-publicc-photos-dev.s3.amazonaws.com"],
     },
